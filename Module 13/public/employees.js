cd310827e6983d185e96bd6128a431914543c2e7
Ronan Jared Borja <timesnewronan@Ronans-MacBook-Air.local>
2024-05-07 19:01:27 -0700

    Fixed Lab 13
    Module 13

--- Module 13/public/employees.js
+++ Module 13/public/employees.js
@@ -3,10 +3,11 @@ let fartman = 'Fartman';
 let lauren = 'Lauren Tu';
 let dha = 'dobby';
 let sak = 'big sak';
-const element = /*#__PURE__*/React.createElement("ul", {
+const title = "Employees";
+const element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, title), /*#__PURE__*/React.createElement("ul", {
   style: {
     'color': 'green',
     'fontSize': '24px'
   }
-}, /*#__PURE__*/React.createElement("li", null, ronan), /*#__PURE__*/React.createElement("li", null, fartman), /*#__PURE__*/React.createElement("li", null, lauren), /*#__PURE__*/React.createElement("li", null, dha), /*#__PURE__*/React.createElement("li", null, sak));
+}, /*#__PURE__*/React.createElement("li", null, ronan), /*#__PURE__*/React.createElement("li", null, fartman), /*#__PURE__*/React.createElement("li", null, lauren), /*#__PURE__*/React.createElement("li", null, dha), /*#__PURE__*/React.createElement("li", null, sak)));
 ReactDOM.render(element, document.getElementById('content'));
--- Module 13/src/employees.jsx
+++ Module 13/src/employees.jsx
@@ -3,13 +3,17 @@ let fartman = 'Fartman'
 let lauren = 'Lauren Tu'
 let dha = 'dobby'
 let sak = 'big sak'
+const title = "Employees"
 const element = (
-    <ul style = {{'color': 'green', 'fontSize':'24px'}} >
-        <li>{ronan}</li>
-        <li>{fartman}</li>
-        <li>{lauren}</li>
-        <li>{dha}</li>
-        <li>{sak}</li>
-    </ul>
+    <div>
+        <h1>{title}</h1>
+        <ul style = {{'color': 'green', 'fontSize':'24px'}} >
+            <li>{ronan}</li>
+            <li>{fartman}</li>
+            <li>{lauren}</li>
+            <li>{dha}</li>
+            <li>{sak}</li>
+        </ul>
+    </div>
 )
 ReactDOM.render(element, document.getElementById('content'))
